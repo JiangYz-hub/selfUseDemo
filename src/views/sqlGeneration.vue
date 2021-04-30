2f2cae2d78329e2e50178bdacd53cde170e9d61a
yanzhu.jiang <yanzhu.jiang@accenture.com>
2021-04-30 17:15:16 +0800

    lodash i

--- src/views/sqlGeneration.vue
+++ src/views/sqlGeneration.vue
@@ -4,7 +4,8 @@
       class="upload"
       action="none"
       :auto-upload="false"
-      :on-change="checkFile">
+      :on-change="checkFile"
+      :show-file-list="false">
       <el-button size="small" type="primary">点击上传</el-button>
       <div slot="tip" class="el-upload__tip">upload__tip</div>
     </el-upload>
@@ -13,7 +14,7 @@
 </template>
 
 <script>
-  import { upload, download } from '../api/sqlGeneration';
+  import { upload } from '../api/sqlGeneration';
   export default {
     name: "sqlGeneration",
     data() {
@@ -22,24 +23,22 @@
       }
     },
     methods: {
-      checkFile(file, fileList) {
-        console.log(file);
-        const fileType = file.name.substring(file.name.lastIndexOf('.')+1);
-        console.log(fileType);
-        if (fileType !== 'xlsx' || fileType !== 'xls') {
-          // return alert('文件格式不正确，只允许上传excel文件');
-          return message(this, '文件格式不正确，只允许上传excel文件', 'error');
+      async checkFile(file, fileList) {
+        const fileType = await file.name.substring(file.name.lastIndexOf('.')+1).toLowerCase();
+        const allowedTypes=['xlsx', 'xls'];
+        if (!_.includes(allowedTypes, fileType)){
+          return console.log('文件格式不正确');
+          // return message(this, '文件格式不正确，只允许上传excel文件', 'error');
         }
-        if(file.size / 1024 / 1024 / 2) {
-          // return alert('文件不能超过2M');
-          return message(this, '文件不能超过2M', 'error');
+        if(file.size / 1024 / 1024 > 2) {
+          return console.log('文件不能超过2M');
+          // return message(this, '文件不能超过2M', 'error');
         }
         this.uploadFiles = file;
         this.handleUpload(file);
       },
       handleUpload(file) {
         let formData = new FormData;
-        console.log(file);
         formData.append(file, file.raw);
         upload(formData).then(res => console.log('上传成功')).catch(err => console.log('上传失败'));
         // upload(formData).then(res => message(this, '上传成功')).catch(err => message(this, '上传失败', 'error'));
